a60d929977345bb9d8469f45d4da10a4edc79d1a
Karolina Nocek <knocek123@o2.pl>
2025-11-04 16:58:25 +0100

    registration and login page css update

--- src/pages/login_page.jsx
+++ src/pages/login_page.jsx
@@ -5,16 +5,22 @@ import { Link } from "react-router-dom";
 const Login_Page = () =>{
     return(
         <div className="loginBackground">
+            <div className="loginContent">
+            <Link to="/start_page" className="logoLink">
+                <img src="/images/logo.png" alt="TEMPLE OF GAINS" className="loginLogo" />
+            </Link>
             <div className="loginBox">
                 <h2>LOGOWANIE</h2>
-                <form className="loginForm">
-                    <input id="login" type="login" placeholder="login"></input>
-                    <br></br>
-                    <input id="haslo" type="password" placeholder="haslo"></input>
+                <form className="loginForm" autoComplete="off">
+                    <label htmlFor ="username">Nazwa użytkownika</label>
+                    <input id="username" type="text" placeholder="login"></input>
+                    <label htmlFor ="password">Hasło</label>
+                    <input id="password" type="password" placeholder="haslo"></input>
                 </form>
 
                 <Link to="/dashboard" className="logRegGoToBtn" onClick={testFn}>Zaloguj!</Link>
                 <p>Nie posiadasz konta? <Link to="/register">Zarejestruj się!</Link></p>
+            </div>
             </div>
         </div>
     );
--- src/pages/register_page.jsx
+++ src/pages/register_page.jsx
@@ -5,19 +5,26 @@ import { Link } from "react-router-dom";
 const Register_Page = () =>{
     return(
         <div className="loginBackground">
+        <div className="loginContent">
+        <Link to="/start_page" className="logoLink">
+            <img src="/images/logo.png" alt="TEMPLE OF GAINS" className="loginLogo" />
+        </Link>
         <div className="loginBox">
             <h2>REJESTRACJA</h2>
-            <h3>TEMPLE_ OF GAINS</h3>
-            <form className="loginForm">
-                <input id="login" type="login" placeholder="login"></input>
-                <br></br>
-                <input id="login" type="email" placeholder="email"></input>
-                <br></br>
-                <input id="haslo" type="password" placeholder="haslo"></input>
+            <form className="loginForm" autoComplete="off">
+                <label htmlFor ="username">Nazwa użytkownika</label>
+                <input id="username" type="text" placeholder="nazwa użytkownika"></input>
+                <label htmlFor ="email">Adres e-mail</label>
+                <input id="email" type="email" placeholder="adres e-mail"></input>
+                <label htmlFor ="password">Hasło</label>
+                <input id="password" type="password" placeholder="hasło"></input>
+                <label htmlFor ="confirm-password">Potwierdź hasło</label>
+                <input id="confirm-password" type="password" placeholder="potwierdź hasło"></input>
             </form>
 
             <Link to="/dashboard" className="logRegGoToBtn" onClick={testFn}>Zarejestruj się!</Link>
             <p>*Zakładając konto akceptujesz regulamin.</p>
+        </div>
         </div>
     </div>
     );
