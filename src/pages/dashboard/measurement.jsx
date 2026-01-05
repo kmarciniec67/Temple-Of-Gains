d51d67deaa7da99cddb0ceeb49d5ceaf1b6660f5
knocek <knocek123@o2.pl>
2026-01-05 23:06:34 +0100

    correct

--- src/pages/dashboard/measurement.jsx
+++ src/pages/dashboard/measurement.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "./measurement.module.css";
 import { useNavigate } from "react-router-dom";
 import useMeasurements from "../../hooks/useMeasurements";
